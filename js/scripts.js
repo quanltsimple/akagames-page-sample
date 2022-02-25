9734e6b762bfd51ae36d996fc6f99d1501be3582
Quan Le Trong <44834926+quanltsimple@users.noreply.github.com>
2022-02-25 23:18:50 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@
 
 
     /* Countdown Timer - The Final Countdown */
-	$('#clock').countdown('2022/02/01 00:00:00') /* change here your "countdown to" date */
+	$('#clock').countdown('2022/06/01 00:00:00') /* change here your "countdown to" date */
 	.on('update.countdown', function(event) {
 		var format = '<span class="counter-number">%D<br><span class="timer-text">Days</span></span><span class="counter-number">%H<br><span class="timer-text">Hours</span></span><span class="counter-number">%M<br><span class="timer-text">Minutes</span></span><span class="counter-number">%S<br><span class="timer-text">Seconds</span></span>';
 		$(this).html(event.strftime(format));
